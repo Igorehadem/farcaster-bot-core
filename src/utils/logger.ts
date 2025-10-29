8ca23b103de4541fedace78e4f5780784d4a7d9f
Igorehadem <140109468+Igorehadem@users.noreply.github.com>
2025-10-29 09:37:35 +0300

    Delete src/utils/logger.ts

--- src/utils/logger.ts
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-
-export function logCast(message: string) {
-  const line = `[${new Date().toISOString()}] ${message}\n`;
-  fs.appendFileSync("cast-log.txt", line);
-  console.log(`🪶 Logged: ${message}`);
-}
